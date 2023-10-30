19cfc3221560934c7d908860bb8f147b35493629
Homework1 <kalinina_en@hotmail.com>
2023-10-30 22:33:51 +0300

    Initial commit

---
+++ src/main/java/BuratinosApples.java
@@ -0,0 +1,14 @@
+public class BuratinosApples {
+
+    public static void main(String[] args) {
+
+        final int buratinosApples = 10;
+        System.out.println("buratinosApples = " + buratinosApples);
+
+        final int karabasesApples = 6;
+        System.out.println("karabasesApples = " + karabasesApples);
+
+        final int remainsOfApples = buratinosApples - karabasesApples;
+        System.out.println("remainsOfApples = " + remainsOfApples);
+    }
+}
